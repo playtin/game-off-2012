c14f07ae9c02d132e901091f46213967f3436235
Francois Laptop <webarbeit@gmail.com>
2012-11-01 12:17:55 +0100

    Added endless mode. Still issue with player reset

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,55 @@
+$(document).ready(function() {
+
+  // Toggle endless mode
+  $("#endlessButton").click(function() {
+
+    var btn = $(this);
+    var activeClass = "btn-danger";
+
+    btn.toggleClass(activeClass).toggleClass("btn-inverse");
+
+    if( btn.hasClass(activeClass) ) {
+      btn.html("Endless mode is on");
+    } else {
+      btn.html("Endless mode is off");
+    }
+
+  });
+
+});
+
+
+var isEndlessMode = function() {
+
+  return $("#endlessButton").hasClass("btn-danger");
+
+};
+
+
+function randomGame(player) {
+
+  if(!isEndlessMode()) return false;
+
+  console.log("next game in 5 seconds");
+
+  var random_index = Math.floor( Math.random() * (game_data.length - 1) );
+  
+  player.reset();
+
+  // Show 5 Seconds countdown ...
+
+  setTimeout(function() {
+      player_main( game_data[random_index].data, game_data[random_index].id);
+  }, 5000);  
+
+}
+
 function player_main( data, game_id ) {
   
   game_id = game_id || 0;
-  
+
+  window.player = new Player();  
+
   function increaseCounter( _isWin ) {
     
     $.ajax({
@@ -9,11 +57,11 @@ function player_main( data, game_id ) {
       data : { win : _isWin },
       type : 'PUT',
       success : function() {}
-    });
-    
-  };
-  
-  window.player = new Player();  
+    }); 
+
+    randomGame(window.player);
+
+  };  
   
   console.log( JSON.stringify( data ) );
   
